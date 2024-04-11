4f166c0b3fc3253c80dfc5a34f38b7c7050f8f29
Tristan vera <tristan.vera19@gmail.com>
2024-04-11 09:39:39 +0200

    Changing nextauht

--- next.config.js
+++ next.config.js
@@ -18,8 +18,9 @@ module.exports = (env) => {
           "mongodb+srv://tristanvera19:NiFNnRRW6RFzviYX@cluster0.slpqrdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         MONGODB_DATABASE: "threads2",
         NEXTAUTH_SECRET: "jqkesgfiuzfgqzifzpergfhpzpigh",
-        NEXTAUTH_URL: "http://localhost:3000",
+        NEXTAUTH_URL: "https://threads-lyart.vercel.app/",
       },
     };
   }
 };
+s;
