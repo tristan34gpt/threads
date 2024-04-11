5db9bf01d447384bae17deb082736f58021b4a5a
Tristan vera <tristan.vera19@gmail.com>
2024-04-11 10:12:05 +0200

    modification url

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ module.exports = (env) => {
           "mongodb+srv://tristanvera19:NiFNnRRW6RFzviYX@cluster0.slpqrdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         MONGODB_DATABASE: "threads2",
         NEXTAUTH_SECRET: "jqkesgfiuzfgqzifzpergfhpzpigh",
-        NEXTAUTH_URL: "http://localhost:3000",
+        NEXTAUTH_URL: "https://threads-two-jet.vercel.app",
       },
     };
   }
